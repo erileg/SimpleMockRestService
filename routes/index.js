086ed4c7c33a8dc07a56a40f3315a55d1c9cc006
erle <erik.legenhausen@gmail.com>
2016-11-20 15:36:12 +0100

    using curly braces to indicate query parameter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = function(app) {
 		var urls = app._router.stack.filter((r) => {
 			return r.route && r.route.path && r.route.path !== "/";
 		}).map((r) => {
-			return `${req.protocol}://${req.hostname}:${req.socket.localPort}${r.route.path.replace(/:(\w+)/g, "[$1]")}`;
+			return `${req.protocol}://${req.hostname}:${req.socket.localPort}${r.route.path.replace(/:(\w+)/g, "{$1}")}`;
 		});
 
 		res.render("index", {
