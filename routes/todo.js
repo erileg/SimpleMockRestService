88b4d7ab7a86b93eb337684372637cb84290714a
erle <erik.legenhausen@gmail.com>
2016-11-22 22:29:27 +0100

    forward mongodb errors to standard error handler

--- routes/todo.js
+++ routes/todo.js
@@ -2,12 +2,12 @@ var Todo = require("../models/todo");
 
 module.exports = app => {
     // show all todos
-    app.get('/api/todos', (req, res) => {
+    app.get('/api/todos', (req, res, next) => {
         // use mongoose to get all todos in the database
         Todo.find(function (err, todos) {
             // if there is an error retrieving, send the error. nothing after res.send(err) will execute
             if (err)
-                res.send(err)
+                next(err, req, res);
 
             res.format({
                 html: function () {
@@ -25,35 +25,35 @@ module.exports = app => {
     });
 
     // create todo and send back all todos after creation
-    app.post('/api/todos', (req, res) => {
+    app.post('/api/todos', (req, res, next) => {
         Todo.create({
             text: req.body.text,
             done: false
         }, function (err, todo) {
             if (err)
-                res.send(err);
+                next(err, req, res);
 
             // get and return all the todos after you create another
             Todo.find(function (err, todos) {
                 if (err)
-                    res.send(err)
+                    next(err, req, res);
                 res.json(todos);
             });
         });
     });
 
     // delete a todo
-    app.delete('/api/todos/:todoId', (req, res) => {
+    app.delete('/api/todos/:todoId', (req, res, next) => {
         Todo.remove({
             _id: req.params.todoId
         }, function (err, todo) {
             if (err)
-                res.send(err);
+                next(err, req, res);
 
             // get and return all the todos after you removed another
             Todo.find(function (err, todos) {
                 if (err)
-                    res.send(err)
+                    next(err, req, res);
                 res.json(todos);
             });
         });
